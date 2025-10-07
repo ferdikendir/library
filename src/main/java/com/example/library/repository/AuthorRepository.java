124dc019ac59f96da0b462b563b43ef922533e10
ferdikendir <ferdi.kendir@std.yildiz.edu.tr>
2025-10-07 21:56:09 +0300

    (feature/Book) - Added Author

---
+++ src/main/java/com/example/library/repository/AuthorRepository.java
@@ -0,0 +1,13 @@
+package com.example.library.repository;
+
+import com.example.library.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AuthorRepository  extends JpaRepository<Author, UUID> {
+    Optional<Author> findById(UUID id);
+
+    Author getFirstByName(String name);
+}
