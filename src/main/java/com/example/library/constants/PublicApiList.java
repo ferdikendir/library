cb9d20af79974a233dd0bbf2e198b4169e2d0098
ferdikendir <ferdi.kendir@std.yildiz.edu.tr>
2025-10-07 21:55:47 +0300

    (feature/Book) - Added mock data

---
+++ src/main/java/com/example/library/constants/PublicApiList.java
@@ -0,0 +1,16 @@
+package com.example.library.constants;
+
+
+import java.util.List;
+
+public class PublicApiList {
+
+    private static final List<String> PUBLIC_API_LIST = List.of(
+            "/api/auth/login",
+            "/api/auth/register"
+    );
+
+    public static List<String> getPublicApiList() {
+        return PUBLIC_API_LIST;
+    }
+}
